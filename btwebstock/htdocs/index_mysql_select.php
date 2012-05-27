e01e920f58a9ec93ad0d269dc28938271e7210da
Sasa Ostrouska <casaxa@gmail.com>
2012-05-27 18:06:02 -0300

    Added back index_psql.php.

---
+++ btwebstock/htdocs/index_mysql_select.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * Example of database use
+ *
+ */
+$debug = 0;
+
+$currentdir = getcwd();
+$includesdir = $currentdir."/classes/";
+
+if ($debug){
+	echo $includesdir."<br>";
+}
+
+include_once($includesdir."class_database.inc");
+
+$_db = new mysql();
+$_db->SetServer('localhost');
+$_db->SetDBase('db_btwebstock');
+$_db->SetUser('root');
+$_db->SetPassword('mysqlrootpa55word');
+$_db->SetPort(3306);
+//var_dump($_db->Connect());
+//	echo "You have fucked up something. Go and debug.";
+$_db->Connect();
+
+$_sql = "SELECT * FROM tab_test";
+$_db->executeSQL($_sql);
+
+echo "Number of registers returned by SELECT:{$_db->getNumRows()}<br>";
+echo "<table border=1 cellpadding=5 width=400>
+	<tr><th>Code</th><th>Description</th><th>Value</th></tr>";
+while($_d = $_db->nextPos()) {
+	echo "<tr><td>{$_d['code']}<td>
+		<td>{$_d['description']}</td>
+		<td align='right'>{$_d['value']}</td></tr>";
+}
+echo "</table>";
+
+?>
