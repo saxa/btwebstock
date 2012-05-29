fa5aeccc9ebc94c14de5646ff2d2e380006075d3
Sasa Ostrouska <casaxa@gmail.com>
2012-05-28 23:39:18 -0300

    Fixed in index_mysql_select.php the visual appearance of the code. It is not showing the result yet.

--- btwebstock/htdocs/index_mysql_select.php
+++ btwebstock/htdocs/index_mysql_select.php
@@ -4,7 +4,7 @@
  * Example of database use
  *
  */
-$debug = 0;
+$debug = 1;
 
 $currentdir = getcwd();
 $includesdir = $currentdir."/classes/";
@@ -17,24 +17,28 @@ include_once($includesdir."class_database.inc");
 
 $_db = new mysql();
 $_db->SetServer('localhost');
-$_db->SetDBase('db_btwebstock');
+$_db->SetDBase('db_btwebstock_test');
 $_db->SetUser('root');
 $_db->SetPassword('mysqlrootpa55word');
 $_db->SetPort(3306);
-//var_dump($_db->Connect());
-//	echo "You have fucked up something. Go and debug.";
 $_db->Connect();
 
-$_sql = "SELECT * FROM tab_test";
+$_sql = "SELECT * FROM table_test";
 $_db->executeSQL($_sql);
 
 echo "Number of registers returned by SELECT:{$_db->getNumRows()}<br>";
 echo "<table border=1 cellpadding=5 width=400>
-	<tr><th>Code</th><th>Description</th><th>Value</th></tr>";
+	<tr>
+		<th>Code</th>
+		<th>Description</th>
+		<th>Value</th>
+	</tr>";
 while($_d = $_db->nextPos()) {
-	echo "<tr><td>{$_d['code']}<td>
-		<td>{$_d['description']}</td>
-		<td align='right'>{$_d['value']}</td></tr>";
+	echo "	<tr>
+			<td>{$_d['code']}</td>
+			<td>{$_d['description']}</td>
+			<td align='right'>{$_d['value']}</td>
+		</tr>";
 }
 echo "</table>";
 
