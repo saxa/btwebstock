bdfd70a21a84d2c8a4de8acc12603db5594b569c
Sasa Ostrouska <casaxa@gmail.com>
2012-05-29 23:14:26 -0300

    Fixed the index_pgsql.php file.

--- btwebstock/htdocs/index_psql.php
+++ btwebstock/htdocs/index_psql.php
@@ -21,28 +21,31 @@ $_db->SetDBase('db_btwebstock');
 $_db->SetUser('postgres');
 $_db->SetPassword('postgres');
 $_db->SetPort(5432);
-var_dump($_db->Connect());
+/* if (var_dump($_db->Connect())) {
 	echo "You have fucked up something. Go and debug.";
-//$_db->Connect();
-/*
-$_sql = "create table tab_test(code int4 default 0, description varchar(20), valor float, primary key(code))";
-if($_db->executeSQL($_sql)!== false) {
+} */
+$_db->Connect();
+
+$_sql = "create table tab_test (code int4 default 0, description varchar(20), value float, primary key(code))";
+if($_db->executeSQL($_sql) !== false) {
 	echo "Table sucessfully created !<br>";
 }
 
-$_sql = "insert into tab_test values (1, 'test', 1,5)";
+$_sql = "insert into tab_test (code, description, value) values (1, 'test', 1.5)";
 if($_db->executeSQL($_sql) !== false) {
 	echo "1st register inserted.<br>";
 }
 
-$_sql = "insert into tab_test values (2, 'test', 3,5)";
+$_sql = "insert into tab_test (code, description, value) values (2, 'test', 3.5)";
 if($_db->executeSQL($_sql) !== false) {
 	echo "2nd register inserted.<br>";
 }
-$_sql = "insert into tab_test values (3, 'test', 7,15)";
+
+$_sql = "insert into tab_test (code, description, value) values (3, 'test', 7.15)";
 if($_db->executeSQL($_sql) !== false) {
 	echo "3rd register inserted.<br>";
 }
- */
+
 
 ?>
+
